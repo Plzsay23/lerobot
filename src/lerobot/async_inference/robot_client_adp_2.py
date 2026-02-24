2987b531ce1b5be8970c7a15c62d415d66fa9660
김범수 <kjt132kmsa@gmail.com>
2026-02-25 00:38:58 +0900

    Update robot_client_adp_2.py

--- src/lerobot/async_inference/robot_client_adp_2.py
+++ src/lerobot/async_inference/robot_client_adp_2.py
@@ -17,11 +17,7 @@ from lerobot.cameras.realsense.configuration_realsense import RealSenseCameraCon
 from lerobot.robots import (  # noqa: F401
 Robot,
 RobotConfig,
-bi_so_follower,
-koch_follower,
 make_robot_from_config,
-omx_follower,
-so_follower,
 )
 from lerobot.transport import (
 services_pb2,  # type: ignore
